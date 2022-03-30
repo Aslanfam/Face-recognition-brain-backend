46b10ac35d86c1d852cb54d80f23ed3eeb9ef059
Alassane Fofana <74676173+Las07@users.noreply.github.com>
2022-03-30 18:45:27 +0200

    database update

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    host : 'postgresql-lively-89885',
-    user : 'alassane',
-    password : '',
-    database : 'smartbrain'
+    connectionString : 'process.env.DATABASE_URL',
+    ssl: true
   }
 });
 
